6344b8653c0c415b51b1fb7bf54e1d7f63dd19b6
lzil <liang.zhou.18@ucl.ac.uk>
2020-02-20 11:22:57 +0000

    figures 2

--- draw.py
+++ draw.py
@@ -6,8 +6,8 @@ import os
 
 import matplotlib.pyplot as plt
 import json
-plt.xkcd()
-plt.rc('axes', prop_cycle=(cycler(color=['r', 'g', 'purple', 'orchid'])))
+#plt.xkcd()
+plt.rc('axes', prop_cycle=(cycler(color=['r', 'g', 'purple', 'orchid', 'lightblue'])))
 
 on_sshfs = True
 
@@ -23,7 +23,7 @@ model_map = {
 
 import matplotlib as mpl
 mpl.rc('font', family='Helvetica Neue')
-mpl.rc('font', family='Humor Sans')
+#mpl.rc('font', family='Humor Sans')
 def main(d_types):
     plt.figure(figsize=(8,7))
     for d in d_types:
@@ -37,23 +37,25 @@ def main(d_types):
             fid_data = json.load(f)
 
         ts, fid_train, fid_test = fid_data
-        ts = ts[:-1]
+        ts = ts[:len(fid_test)]
 
 
         # plt.plot(x_range, fid_train, '.', linestyle='-', label=model_map[d], lw=2)
 
-        plt.plot(ts, fid_test, 'o', linestyle='-.', label=model_map[d], lw=3)
+        plt.plot(ts, fid_test, '.', linestyle='-', label=model_map[d], lw=2)
 
     #plt.plot(x_range, np.ones_like(x_range) * fid_test[0], linestyle='dotted', c='gray', lw=1)
     
     plt.axhline(y=fid_test[0], linestyle='dotted', color='gray', linewidth=2, xmin=-100,xmax=100)
     plt.xlabel('number of LMC steps')
     plt.ylabel('FID score')
-    plt.ylim([30,37])
+    pmin = 30
+    pmax = 70
+    plt.ylim([pmin, pmax])
 
-    plt.yticks(np.arange(30, 37, 1))
+    plt.yticks(np.arange(pmin, pmax, 1))
 
-    plt.grid(which='major', axis='y', linewidth=0.2)
+    plt.grid(which='major', axis='both', linewidth=0.2)
     plt.minorticks_on()
 
     plt.legend()
@@ -71,9 +73,9 @@ if __name__ == '__main__':
 
 
     d_types = ['dcgan',
-                #'dcgan-sn',
-                #'dcgan-sn_gp-10_lr-2e5',
+                'dcgan-sn',
+                'dcgan-sn_gp-10_lr-2e5',
                 'resnet_gp-10',
-                #'resnet-sn_gp-10'
+                'resnet-sn_gp-10'
                 ]
     main(d_types)
